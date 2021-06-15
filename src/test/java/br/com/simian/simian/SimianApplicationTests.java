321e1276d4d69da229b03ebc8591afb1263f650c
IrlanGomesPessoal <irlan.carlo@gmail.com>
2021-06-15 10:00:14 -0400

    primeiro commit

---
+++ src/test/java/br/com/simian/simian/SimianApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.simian.simian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
